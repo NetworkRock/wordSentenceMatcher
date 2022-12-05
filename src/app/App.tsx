bd7ac4894d174594d4b2253887fb1a2d59b342cb
RobinLeeb <R.leeb@gmx.de>
2022-12-05 15:24:09 +0100

    feat: Add a button to fire a post call with data
    - Created a button and a handleRequest method for sending a POST request
      to the node server
    
    feat: Add a backend error component for showing more nicely backend
    errors

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,7 +1,41 @@
-import React from "react";
+import { useState } from "react";
+
+// Models
+import { BackendErrorType } from './models/BackendErrorType';
+
+// Components
+import BackendError from './components/BackendError';
+
+// Styles
 import "./App.css";
 
 function App() {
+
+  const [result, setResult] = useState('')
+  const [error, setError] = useState<BackendErrorType>()
+
+  const handleRequest = async (searchTerm: string, text: string) => {
+    let response: Response
+    const data = {searchTerm, text}
+    try {
+      response = await fetch("http://localhost:8000/check", { 
+        method: "POST",
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        cache: 'no-cache',
+        body: JSON.stringify(data)
+      })
+      await response.json().then((data: { message: string }) => setResult(data.message))
+    } catch (error) {
+      setError(error as unknown as BackendErrorType)
+    }
+  }
+
+  if(error) {
+    return <BackendError message={error.message} />
+  }
+
   return (
     <div className="App">
       <header className="App-header">
@@ -10,6 +44,8 @@ function App() {
       </header>
       <main className='App-main'>
         <textarea placeholder="Write here your text which should be check by overlaps" className='App-textarea'></textarea>
+        <button onClick={() => handleRequest('SEARCH_TEXT', 'TEXT')}>Find matches</button>
+        <p>{result}</p>
       </main>
     </div>
   );
