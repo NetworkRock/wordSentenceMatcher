3fdba91f70e7662750f41e56d6ab77bd58d92ed6
RobinLeeb <R.leeb@gmx.de>
2022-12-06 10:14:37 +0100

    feat: Complete the logic in the algorythm
    - Include logic for printing the overlapping characters

--- src/api/index.ts
+++ src/api/index.ts
@@ -21,35 +21,57 @@ app.get("/", (req: Request, res: Response) => {
   res.send("Healthy");
 });
 
+function max(a: number, b: number) {
+  if (a > b) return a;
+  else return b;
+}
+
+// This algorithm works and could be improved in future with memorication algorithm
+// to increase performance from O(2^n) to O(mn)
 app.post("/check", (req, res) => {
-  const searchTerm = req.body.searchTerm;
-  const text = req.body.text;
-  let dp: number[][] = [[]];
-  let subSequence: string[] = [];
-
-  for (let i = 0; i < searchTerm.length + 1; i++) {
-    dp[i] = [];
-    for (let j = 0; j < text.length + 1; j++) {
-      dp[i][j] = 0;
-    }
+  console.log(req.body)
+  const str1 = req.body.searchTerm
+  const str2 = req.body.text
+  let len1 = str1.length;
+  let len2 = str2.length;
+  let lcs = new Array(len1 + 1);
+  for (let i = 0; i <= len1; i++) {
+    lcs[i] = new Array(len2 + 1);
   }
-
-  for (let j = text.length - 1; j > -1; j--) {
-    for (let i = searchTerm.length - 1; i > -1; i--) {
-      if (searchTerm[i] === text[j]) {
-        // TODO: Find a way to add the overlapping sequence
-        // console.log(searchTerm[i])
-        // console.log(text[j])
-        dp[i][j] = 1 + dp[i + 1][j + 1];
+  for (let i = 0; i <= len1; i++) {
+    for (let j = 0; j <= len2; j++) {
+      if (i == 0 || j == 0) {
+        lcs[i][j] = 0;
       } else {
-        dp[i][j] = Math.max(dp[i][j + 1], dp[i + 1][j]);
+        if (str1[i - 1] == str2[j - 1]) {
+          lcs[i][j] = 1 + lcs[i - 1][j - 1];
+        } else {
+          lcs[i][j] = max(lcs[i][j - 1], lcs[i - 1][j]);
+        }
       }
     }
   }
-  const subString = subSequence.join("");
+
+  let n = lcs[len1][len2];
+  let str = "";
+  let i = len1;
+  let j = len2;
+  while (i > 0 && j > 0) {
+    if (str1[i - 1] == str2[j - 1]) {
+      str += str1[i - 1];
+      i--;
+      j--;
+    } else {
+      if (lcs[i][j - 1] > lcs[i - 1][j]) {
+        j--;
+      } else {
+        i--;
+      }
+    }
+  }
   res.json({
-    numberOverlapping: dp[0][0],
-    charactersOverlapping: subString,
+    numberOverlapping: n,
+    charactersOverlapping: str.split('').reverse().join(''),
   } as CheckResponseType);
 });
 
